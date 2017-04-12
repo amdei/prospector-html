764c6850d45ab5cf75fa2be443513edfa137e17d
Andrey Kulikov <amdei@cryptopro.ru>
2017-04-12 20:09:10 +0300

    Initial skeleton works.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # prospector-html
-HTML report generator from prospector static analyzer tool JSON output.
+HTML report generator from [prospector](https://blog.landscape.io/prospector-python-static-analysis-for-humans.html) static analyzer tool JSON output.
+
+
+## Synopsis
+    pip install json2html
+    cd <python-project-sources-dir>
+    prospector --no-style-warnings --strictness medium --output-format json > prospector_report.json
+    prospector-html --input prospector_report.json
+    cat report.html
---
+++ prospector-html.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+from json2html import *
+
+
+def get_report_body(obj):
+    return json2html.convert(json=obj, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='input JSON file name', required=True, action='store', type=str)
+args = parser.parse_args()
+
+print  args.input
+
+
+with open(args.input, 'r') as f:
+    json_str = f.read()
+
+json_obj = json.loads(json_str)
+
+html_string = '''
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <style>body{ margin:0 100; background:whitesmoke; }</style>
+    </head>
+    <body>
+''' + get_report_body(json_obj['messages']) + '''
+    </body>
+</html>'''
+
+f = open('report.html','w')
+f.write(html_string)
+f.close()
