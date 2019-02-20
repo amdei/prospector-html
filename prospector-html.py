e4d17036eb1ab1aaadd871eccedaa04306fc764f
Igr <igron99@mail.ru>
2019-02-20 11:36:27 +0300

    Sort by filename and line.
    Sort messages by filename and line.

--- prospector-html.py
+++ prospector-html.py
@@ -8,17 +8,19 @@ from json2html import *
 def get_report_body(obj):
     return json2html.convert(json=obj, table_attributes="id=\"info-table\" class=\"table table-bordered table-hover\"")
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', help='input JSON file name', required=True, action='store', type=str)
 args = parser.parse_args()
 
-print  args.input
+print args.input
 
 
 with open(args.input, 'r') as f:
     json_str = f.read()
 
 json_obj = json.loads(json_str)
+json_obj['messages'].sort(key=lambda x: x['location']['path'] + ":" + str(x['location']['line']))
 
 html_string = '''
 <html>
@@ -31,6 +33,5 @@ html_string = '''
     </body>
 </html>'''
 
-f = open('report.html','w')
-f.write(html_string)
-f.close()
+with open('report.html', 'w') as f:
+    f.write(html_string)
