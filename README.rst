124a58ca43812cd80d10779c46477b8c46b6f94c
amdei <amdei@users.noreply.github.com>
2020-05-04 00:56:53 +0300

    Prepare pypi package. (#3)

---
+++ README.rst
@@ -0,0 +1,48 @@
+prospector-html
+===============
+
+HTML report generator from
+`prospector <https://blog.landscape.io/prospector-python-static-analysis-for-humans.html>`__
+static analyzer tool JSON output.
+
+Synopsis
+--------
+
+::
+
+    pip install prospector-html
+    cd <python-project-sources-dir>
+    prospector --no-style-warnings --strictness medium --output-format json > prospector_report.json
+    prospector-html --input prospector_report.json
+    cat report.html
+
+Message filtering
+-----------------
+
+Sometimes it is necessary to filter prospector result by content of the
+message, rather than filter-out the whole error class by it's
+suppression. For example prospector would always complains at usage of
+``_meta`` member in Django projects.
+
+In this case one could fileter such messages by specifing it in
+prospector-html config file. ``.prospector-html.yaml`` by default. See
+details in the source of sample config or use following example:
+
+::
+
+    # cat .prospector-html.yaml
+    filter:
+      message:
+          # Filter by exact match
+        - "Message to filter"
+        - Oter message to filter
+      message_re:
+          # Regexps to filter
+        - 'Exactly one space required after comma.*'
+        - 'Exactly one space required before assignment.*'
+
+TODO
+----
+
+-  ???.
+
---
+++ setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '1.0.0'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from setuptools import find_packages
+
+_PACKAGES = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name="prospector2html",
+    version=__version__,
+    author="amDei the Botan",
+    author_email="amdeich@gmail.com",
+    maintainer="Andrey Kulikov",
+    maintainer_email="amdeich@gmail.com",
+    license="MIT",
+    url="https://github.com/amdei/prospector-html",
+    description="HTML report generator from prospector static analyzer tool output.",
+    keywords='pylint pyflakes pep8 mccabe frosted prospector static code analysis',
+    long_description=readme,
+    platforms=["any"],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: Unix',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Quality Assurance'
+    ],
+    packages=_PACKAGES,
+    entry_points={
+        'console_scripts': [
+            'prospector-html = prospector2html.__main__:main',
+        ],
+    },
+    install_requires=[
+        'PyYAML',
+        'prospector',
+        'json2html'
+    ]
+)
